82dd50fa7ce41f4037cbaa1548d29c01c3721c73
kavindaperera <perera.kavinda52@gmail.com>
2021-06-10 22:20:02 +0530

    Add MeshifyCore - meshify framework

---
+++ meshify/src/main/java/com/codewizards/meshify/framework/controllers/MeshifyCore.java
@@ -0,0 +1,8 @@
+package com.codewizards.meshify.framework.controllers;
+
+public class MeshifyCore {
+
+    public static final String PREFS_NAME = "com.codewizards.meshify.client";
+    public static final String PREFS_USER_UUID = "com.codewizards.meshify.uuid";
+    public static final String PREFS_API_KEY = "com.codewizards.meshify.API_KEY";
+}
