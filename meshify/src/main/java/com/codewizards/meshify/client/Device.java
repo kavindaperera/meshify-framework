2c6f44b44027430df1af4e7b3868c2fdc46fb07c
kavindaperera <perera.kavinda52@gmail.com>
2021-06-14 13:21:05 +0530

    Add Device class - meshify client

--- meshify/src/main/java/com/codewizards/meshify/client/Device.java
+++ meshify/src/main/java/com/codewizards/meshify/client/Device.java
@@ -1,4 +1,149 @@
 package com.codewizards.meshify.client;
 
-public class Device {
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothDevice;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.Gson;
+
+public class Device implements Parcelable {
+
+    private String deviceName;
+
+    private String deviceAddress;
+
+    private BluetoothDevice bluetoothDevice;
+
+    private String userId;
+
+    private Config.Antenna antennaType;
+
+    private long crc;
+
+    private String sessionId;
+
+    protected Device(Parcel in) {
+        this.deviceName = in.readString();
+        this.deviceAddress = in.readString();
+        this.bluetoothDevice = (BluetoothDevice) in.readParcelable(BluetoothDevice.class.getClassLoader());
+        this.sessionId = in.readString();
+        this.antennaType = (Config.Antenna) in.readValue(Config.Antenna.class.getClassLoader());
+    }
+
+    public Device(String userId) {
+        this.userId = userId;
+    }
+
+    public Device(String deviceName, String deviceAddress, String userId) {
+        this.deviceName = deviceName;
+        this.deviceAddress = deviceAddress;
+        this.userId = userId;
+    }
+
+    @SuppressLint("MissingPermission")
+    public Device(BluetoothDevice bluetoothDevice, boolean isBLE) {
+        this.bluetoothDevice = bluetoothDevice;
+        this.deviceName = bluetoothDevice.getName();
+        this.deviceAddress = bluetoothDevice.getAddress();
+        this.antennaType = isBLE ? Config.Antenna.BLUETOOTH_LE : Config.Antenna.BLUETOOTH;
+    }
+
+    public static final Creator<Device> CREATOR = new Creator<Device>() {
+        @Override
+        public Device createFromParcel(Parcel in) {
+            return new Device(in);
+        }
+
+        @Override
+        public Device[] newArray(int size) {
+            return new Device[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.deviceName);
+        dest.writeString(this.deviceAddress);
+        dest.writeParcelable(this.bluetoothDevice, flags);
+        dest.writeString(this.sessionId);
+        dest.writeValue(this.antennaType);
+    }
+
+    /*getters*/
+
+    public Config.Antenna getAntennaType() {
+        return this.antennaType;
+    }
+
+    public BluetoothDevice getBluetoothDevice() {
+        return this.bluetoothDevice;
+    }
+
+    public long getCrc() {
+        return crc;
+    }
+
+    public String getDeviceAddress() {
+        return this.deviceAddress;
+    }
+
+    public String getDeviceName() {
+        return this.deviceName;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    /*setters*/
+
+    public void setAntennaType(Config.Antenna antennaType) {
+        this.antennaType = antennaType;
+    }
+
+    public void setBluetoothDevice(BluetoothDevice bluetoothDevice) {
+        this.bluetoothDevice = bluetoothDevice;
+    }
+
+    public void setCrc(long crc) {
+        this.crc = crc;
+    }
+
+    public void setDeviceAddress(String deviceAddress) {
+        this.deviceAddress = deviceAddress;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public synchronized void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Device) {
+            Device device = (Device) obj;
+            return device.getDeviceAddress() != null && device.getDeviceAddress().trim().equalsIgnoreCase(getDeviceAddress().trim());
+        }
+        throw new IllegalArgumentException(obj.getClass().getName() + " is not a Device");
+    }
 }
