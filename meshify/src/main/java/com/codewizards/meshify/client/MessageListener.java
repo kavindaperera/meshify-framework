ce6dd852a4709fdc47259c0c7b3f61d495e990e2
JudeRanidu <juderanidu96@gmail.com>
2021-06-30 13:27:50 +0530

    Add javadoc to MessageListener

--- meshify/src/main/java/com/codewizards/meshify/client/MessageListener.java
+++ meshify/src/main/java/com/codewizards/meshify/client/MessageListener.java
@@ -2,11 +2,23 @@ package com.codewizards.meshify.client;
 
 import com.codewizards.meshify.framework.expections.MessageException;
 
+/**
+ * This class allows to define callback functions for Message listening process
+ */
 public abstract class MessageListener {
 
+    /**
+     * Callback when a message was received successfully
+     * @param message The Message object representing the received message
+     */
     public void onMessageReceived(Message message) {
     }
 
+    /**
+     * Callback when a message was not successfully received
+     * @param message The Message object representing the failed message
+     * @param exception The MessageException object captured at runtime
+     */
     public void onMessageFailed(Message message, MessageException exception) {
     }
 
