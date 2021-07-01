893e00c628c7d30e71fe40a819007668ab36d9b8
kavindaperera <perera.kavinda52@gmail.com>
2021-07-02 01:17:23 +0530

    Add SignupActivity

---
+++ app/src/main/java/com/codewizards/meshify_chat/ux/intro/SignupActivity.java
@@ -0,0 +1,19 @@
+package com.codewizards.meshify_chat.ux.intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codewizards.meshify_chat.R;
+
+
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+}
