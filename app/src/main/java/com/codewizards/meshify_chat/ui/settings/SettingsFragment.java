ac460388a7d03c3835baed4cbf51fddaf2a9d68c
kavindaperera <perera.kavinda52@gmail.com>
2021-08-07 13:44:30 +0530

    Add phone verification to settings

--- app/src/main/java/com/codewizards/meshify_chat/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/codewizards/meshify_chat/ui/settings/SettingsFragment.java
@@ -1,14 +1,23 @@
 package com.codewizards.meshify_chat.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
+import com.codewizards.meshify.client.Meshify;
 import com.codewizards.meshify_chat.R;
+import com.codewizards.meshify_chat.auth.MeshifySession;
+import com.codewizards.meshify_chat.ui.intro.IntroActivity;
+import com.codewizards.meshify_chat.ui.signup.SignupActivity;
 import com.codewizards.meshify_chat.util.Constants;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    Preference verification;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -16,5 +25,15 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         manager.setSharedPreferencesName(Constants.PREFS_NAME);
 
         setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        if (MeshifySession.isVerified()) {
+            Preference preference = findPreference("verification");
+            this.verification = preference;
+            preference.setSummary("Verified");
+            preference.setOnPreferenceClickListener(preference1 -> {
+                Toast.makeText(getContext(), "Phone Number Already Verified", Toast.LENGTH_SHORT).show();
+                return false;
+            });
+        }
     }
 }
