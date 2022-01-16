4320cc93da9e21396f2870246070003302049b3f
kavindaperera <perera.kavinda52@gmail.com>
2022-01-16 23:57:20 +0530

    Resolve conflicts

--- meshify/src/main/java/com/codewizards/meshify/framework/controllers/MeshifyUtils.java
+++ meshify/src/main/java/com/codewizards/meshify/framework/controllers/MeshifyUtils.java
@@ -1,16 +1,8 @@
 package com.codewizards.meshify.framework.controllers;
 
-import android.bluetooth.BluetoothGattCharacteristic;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import com.codewizards.meshify.framework.entities.MeshifyEntity;
-import com.codewizards.meshify.framework.entities.MeshifyForwardEntity;
-import com.codewizards.meshify.framework.entities.MeshifyForwardTransaction;
-import com.codewizards.meshify.logs.Log;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MeshifyUtils {
 
@@ -41,40 +33,5 @@ public class MeshifyUtils {
         return result;
     }
 
-    static int method_a(BluetoothGattCharacteristic bluetoothGattCharacteristic) {
-        byte[] arrby = bluetoothGattCharacteristic.getValue();
-        try {
-            return arrby[0];
-        }
-        catch (Exception exception) {
-            return -1;
-        }
-    }
-
-    public static ArrayList<byte[]> getCompressedChunk(MeshifyEntity meshifyEntity) {
-
-        ArrayList<byte[]> arrayList = new ArrayList<byte[]>();
-
-        if (meshifyEntity.getEntity() == 2 ) {
-
-            ArrayList arrayList1 = new ArrayList();
-            MeshifyForwardTransaction meshifyEntityContent = (MeshifyForwardTransaction) meshifyEntity.getContent();
-            meshifyEntity.setContent(null);
-
-            if (meshifyEntityContent.getReach() == null){
-
-                ArrayList arrayList2 = new ArrayList();
-
-                if (arrayList2.size() > 0) {
-
-                    meshifyEntityContent.getMesh().removeAll(arrayList2);
-
-                }
-
-            }
-
-        }
-        return arrayList;
-    }
 
 }
