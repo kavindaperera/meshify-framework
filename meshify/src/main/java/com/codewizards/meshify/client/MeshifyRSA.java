14a268e8427ebeb0b244e41fc25748fb97e2fbe1
kavindaperera <perera.kavinda52@gmail.com>
2022-01-17 00:08:26 +0530

    Reverse commit

--- meshify/src/main/java/com/codewizards/meshify/client/MeshifyRSA.java
+++ meshify/src/main/java/com/codewizards/meshify/client/MeshifyRSA.java
@@ -10,14 +10,10 @@ import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 import javax.crypto.Cipher;
 
@@ -25,9 +21,7 @@ import javax.crypto.Cipher;
  * Can be used to generate Public-Private key pairs for RSA,
  * to encrypt data using Public key and decrypt data using Private key
  */
-public class MeshifyRSA {
-
-    private static final String TAG = "[Meshify][MeshifyRSA]";
+class MeshifyRSA {
 
     /**
      * This will generate a Public-Private key pair for RSA and returns them in a HashMap
@@ -55,8 +49,6 @@ public class MeshifyRSA {
      */
     public static byte[] encrypt (String base64PublicKey, byte[] bytes){
 
-        int totalLength = bytes.length;
-        int chunkSize = 64;
         byte[] encryptedBytes = null;
 
         try {
@@ -68,28 +60,10 @@ public class MeshifyRSA {
             Cipher cipher = Cipher.getInstance("RSA");
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 
-            //encryptedBytes = cipher.doFinal(bytes);
-            List<byte[]> encryptedChunks = new ArrayList<>();
-            int encryptedBytesLength = 0;
-
-            for (int i = 0; i < totalLength; i += chunkSize) {
-                byte[] chunk = Arrays.copyOfRange(bytes, i, i + chunkSize);
-                byte[] encryptedChunk = cipher.doFinal(chunk);
-                encryptedBytesLength += encryptedChunk.length;
-                encryptedChunks.add(encryptedChunk);
-            }
-
-            encryptedBytes = new byte[encryptedBytesLength];
-            int nextIndex = 0;
-
-            for(byte[] encryptedChunk: encryptedChunks){
-                System.arraycopy(encryptedChunk, 0, encryptedBytes, nextIndex, encryptedChunk.length);
-                nextIndex += encryptedChunk.length;
-            }
-
+            encryptedBytes = cipher.doFinal(bytes);
 
         } catch (Exception ex) {
-            Log.d(TAG, ex.getMessage());
+            Log.d("MeshifyRsA.encrypt",ex.getMessage());
             ex.printStackTrace();
         }
         
@@ -106,46 +80,28 @@ public class MeshifyRSA {
      */
     public static byte[] decrypt (String base64PrivateKey, byte[] encryptedBytes){
 
-        int totalLength = encryptedBytes.length;
-        int chunkSize = 256;
         byte[] decryptedBytes = null;
 
         try {
 
             PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decode(base64PrivateKey.getBytes(),0));
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+            PublicKey privateKey = keyFactory.generatePublic(keySpec);
 
             Cipher cipher = Cipher.getInstance("RSA");
             cipher.init(Cipher.DECRYPT_MODE, privateKey);
 
-            //decryptedBytes = cipher.doFinal(encryptedBytes);
-            List<byte[]> decryptedChunks = new ArrayList<>();
-            int decryptedBytesLength = 0;
-
-            for (int i = 0; i < totalLength; i += chunkSize) {
-                byte[] chunk = Arrays.copyOfRange(encryptedBytes, i, i + chunkSize);
-                byte[] decryptedChunk = cipher.doFinal(chunk);
-                decryptedBytesLength += decryptedChunk.length;
-                decryptedChunks.add(decryptedChunk);
-            }
-
-            decryptedBytes = new byte[decryptedBytesLength];
-            int nextIndex = 0;
-
-            for(byte[] decryptedChunk: decryptedChunks){
-                System.arraycopy(decryptedChunk, 0, decryptedBytes, nextIndex, decryptedChunk.length);
-                nextIndex += decryptedChunk.length;
-            }
+            decryptedBytes = cipher.doFinal(encryptedBytes);
 
         } catch (Exception ex) {
-            Log.d(TAG, ex.getMessage());
+            Log.d("MeshifyRsA.decrypt",ex.getMessage());
             ex.printStackTrace();
         }
 
         return decryptedBytes;
 
     }
+
 
     /**
      * Encodes the given byte array as a Base64 encoded String and returns it
